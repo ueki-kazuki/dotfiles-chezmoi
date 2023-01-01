17387ec697ebcf6afbb96efb9787dba877310820
Kazuki <ueki.kazuki@classmethod.jp>
2023-01-02 08:18:24 +0900

    Add config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,7 +5,11 @@ if status is-interactive
     set fish_prompt_hg_status_added "+"
     set fish_color_autosuggestion brblack
     set -x XDG_CONFIG_HOME ~/.config
-    set -x PATH /usr/local/bin $PATH
+    fish_add_path /usr/local/bin
+    fish_add_path ~/go/bin
+    fish_add_path ~/.cargo/bin
+    fish_add_path ~/.deno/bin
+
     set -x NVM_DIR "/usr/local/opt/nvm"
     set -Ux PYENV_ROOT $HOME/.pyenv
     fish_add_path $PYENV_ROOT/bin
@@ -14,26 +18,25 @@ if status is-interactive
     pyenv init - | source
     . (rbenv init - | psub)
     eval (direnv hook fish)
-    set -x PATH $HOME/.cargo/bin:$PATH
     # cf-terraforming completion fish | source
     # ~/bin/chezmoi completion fish | source
 
-    
+
     function fish_user_key_bindings
       bind \cr 'peco_select_history (commandline -b)'
       bind \c] peco_select_ghq_repository
     end
-    
+
     # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
     complete -c aws -f -a '(begin; set -lx COMP_SHELL fish; set -lx COMP_LINE (commandline); /usr/local/bin/aws_completer; end)'
-    
+
     # tabtab source for packages
     # uninstall by removing these lines
     [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
-    
+
     # alias aws='docker run --rm -it -v ~/.aws:/root/.aws -v (pwd):/aws amazon/aws-cli'
-    
-    
+
+
     set -x PIPENV_VENV_IN_PROJECT true
     set -x PIPENV_VERBOSITY -1
     # set pipenv_fish_fancy yes
---
+++ private_dot_config/private_fish/functions/peco_select_ghq_repository.fish
@@ -0,0 +1,16 @@
+function peco_select_ghq_repository
+  set -l query (commandline)
+
+  if test -n $query
+    set peco_flags --query "$query"
+  end
+
+  ghq list | peco $peco_flags | read line
+
+  if [ $line ]
+    # ghq look $line
+    echo "$line"
+    cd (ghq root)/"$line"
+    commandline -f repaint
+  end
+end
