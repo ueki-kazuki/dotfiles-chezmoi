9890342f5ceb5be5150dc787fe486d59dc37e119
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2023-01-04 13:36:09 +0900

    Modify fish config for tabtab, google-cloud-sdk and Java

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -30,6 +30,16 @@ if status is-interactive
     # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
     complete -c aws -f -a '(begin; set -lx COMP_SHELL fish; set -lx COMP_LINE (commandline); /usr/local/bin/aws_completer; end)'
 
+    # tabtab source for serverless package
+    # uninstall by removing these lines or running `tabtab uninstall serverless`
+    [ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+    # tabtab source for sls package
+    # uninstall by removing these lines or running `tabtab uninstall sls`
+    [ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+    # tabtab source for slss package
+    # uninstall by removing these lines or running `tabtab uninstall slss`
+    [ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
+
     # tabtab source for packages
     # uninstall by removing these lines
     [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
@@ -40,6 +50,26 @@ if status is-interactive
     set -x PIPENV_VENV_IN_PROJECT true
     set -x PIPENV_VERBOSITY -1
     # set pipenv_fish_fancy yes
+
+    # curl is keg-only, which means it was not symlinked into /usr/local,
+    # need to have curl first in the PATH
+    set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+
+    # Google Cloud
+    set __google_cloud_sdk "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
+    [ -f $__google_cloud_sdk ]; and source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"; or true
+
+    # openjdk@11 {{{
+    # For the system Java wrappers to find this JDK, symlink it with
+    # sudo ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
+
+    # openjdk@11 is keg-only, which means it was not symlinked into /usr/local,
+    # because this is an alternate version of another formula.
+    set -g fish_user_paths "/usr/local/opt/openjdk@11/bin" $fish_user_paths
+
+    # For compilers to find openjdk@11 you may need to set:
+    set -gx CPPFLAGS "-I/usr/local/opt/openjdk@11/include"
+    # }}}
 end
 
 
