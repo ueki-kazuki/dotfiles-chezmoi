0748b996766a50a1ed8353db89cde78ad66e35f8
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2023-01-24 21:32:41 +0900

    Close nvim-tree buffer with <C-e>

--- private_dot_config/nvim/lua/p-nvimtree.lua
+++ private_dot_config/nvim/lua/p-nvimtree.lua
@@ -6,27 +6,30 @@ vim.g.loaded_netrwPlugin = 1
 
 -- OR setup with some options
 require("nvim-tree").setup({
-	sort_by = "case_sensitive",
-	actions = {
-		open_file = { quit_on_open = true },
-	},
-	update_focused_file = {
-			enable = true
-	},
-	view = {
-		adaptive_size = true,
-		mappings = {
-			list = {
-				{ key = "u", action = "dir_up" },
-			},
-		},
-	},
-	renderer = {
-		group_empty = true,
-	},
-	filters = {
-		dotfiles = true,
-	},
+    sort_by = "case_sensitive",
+    actions = {
+        open_file = { quit_on_open = true },
+    },
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+    },
+    view = {
+        adaptive_size = true,
+        mappings = {
+            list = {
+                { key = "u", action = "dir_up" },
+                { key = "<C-e>", action = "close" },
+            },
+        },
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+    sync_root_with_cwd = false,
 })
 
 vim.keymap.set('n', '<C-e>', '<Cmd>NvimTreeToggle<CR>')
