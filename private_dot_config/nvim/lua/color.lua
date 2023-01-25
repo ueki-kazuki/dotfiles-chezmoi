07ae1836d407a683a8680272493dba4d43cca4c7
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2023-01-25 17:28:27 +0900

    Disable italic for everforest colortheme

--- private_dot_config/nvim/lua/color.lua
+++ private_dot_config/nvim/lua/color.lua
@@ -9,4 +9,10 @@ require('tokyonight').setup({
 
 })
 -- vim.cmd('colorscheme tokyonight')
+vim.cmd('let g:everforest_enable_italic = 0')
+vim.cmd('let g:everforest_disable_italic_comment = 1')
+vim.cmd('let g:everforest_transparent_background = 1')
+vim.cmd('let g:everforest_better_performance = 1')
+
 vim.cmd('colorscheme everforest')
+
