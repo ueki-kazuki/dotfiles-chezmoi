29b67ef63d181a500334528016f203816631e96e
Kazuki Ueki <ueki.kazuki@classmethod.jp>
2023-02-02 21:05:54 +0900

    Configu keymap

--- private_dot_config/nvim/lua/keymap.lua
+++ private_dot_config/nvim/lua/keymap.lua
@@ -2,8 +2,45 @@
 -- vim.g.mapleader = "<Space>"
 vim.g.mapleader = " "
 
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-t><C-p>', ':<C-u>bprev<CR>', opts)
-vim.keymap.set('n', '<C-t><C-n>', ':<C-u>bnext<CR>', opts)
-vim.keymap.set('n', '<C-t>p', ':<C-u>bprev<CR>', opts)
-vim.keymap.set('n', '<C-t>n', ':<C-u>bnext<CR>', opts)
+
+-- see https://alpha2phi.medium.com/neovim-for-beginners-key-mappings-and-whichkey-31dbf58f9f87 
+local keymap = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+local expr_opts = { noremap = true, expr = true, silent = true }
+
+keymap('n', '<C-t><C-p>', ':<C-u>bprev<CR>', default_opts)
+keymap('n', '<C-t><C-n>', ':<C-u>bnext<CR>', default_opts)
+keymap('n', '<C-t>p', ':<C-u>bprev<CR>', default_opts)
+keymap('n', '<C-t>n', ':<C-u>bnext<CR>', default_opts)
+
+-- Center search results
+keymap("n", "n", "nzz", default_opts)
+keymap("n", "N", "Nzz", default_opts)
+
+-- Visual line wraps
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", expr_opts)
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", expr_opts)
+
+-- Better indent
+keymap("v", "<", "<gv", default_opts)
+keymap("v", ">", ">gv", default_opts)
+
+-- Paste over currently selected text without yanking it
+keymap("v", "p", '"_dP', default_opts)
+
+-- Switch buffer
+keymap("n", "<S-h>", ":bprevious<CR>", default_opts)
+keymap("n", "<S-l>", ":bnext<CR>", default_opts)
+
+-- Cancel search highlighting with ESC
+keymap("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_opts)
+
+-- Move selected line / block of text in visual mode
+keymap("x", "K", ":move '<-2<CR>gv-gv", default_opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", default_opts)
+
+-- Resizing panes
+keymap("n", "<Left>", ":vertical resize +1<CR>", default_opts)
+keymap("n", "<Right>", ":vertical resize -1<CR>", default_opts)
+keymap("n", "<Up>", ":resize -1<CR>", default_opts)
+keymap("n", "<Down>", ":resize +1<CR>", default_opts)
